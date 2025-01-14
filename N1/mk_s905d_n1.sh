fba550a19d017b8c17fb13e589de58f039f29393
sck03407 <138302895+sck03407@users.noreply.github.com>
2025-01-14 12:09:16 +0800

    Update mk_s905d_n1.sh

--- N1/mk_s905d_n1.sh
+++ N1/mk_s905d_n1.sh
@@ -172,7 +172,6 @@ adjust_openssl_config
 adjust_getty_config
 adjust_samba_config
 adjust_openssh_config
-adjust_openclash_config
 use_xrayplug_replace_v2rayplug
 create_fstab_config
 adjust_mosdns_config
