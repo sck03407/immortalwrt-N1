d00680f1c41f61ffd645e9cd6fa1b264d870e8aa
sck03407 <138302895+sck03407@users.noreply.github.com>
2025-01-16 16:57:12 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -16,31 +16,31 @@ function git_sparse_clone() {
 # Add packages
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwal
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo package/luci-app-mihomo
 
-#git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
 #git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-
 
 # 加入OpenClash核心
 #chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
 #$GITHUB_WORKSPACE/N1/preset-clash-core.sh
 
-#echo "
+echo "
 # 插件
-#CONFIG_PACKAGE_luci-app-lucky=y
-#" >> .config
+CONFIG_PACKAGE_luci-app-lucky=y
+" >> .config
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.6.6/g' package/base-files/files/bin/config_generate
 
-# 清理主题
+# 清理软件包
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
-
-rm -rf feeds/luci/applications/openwrt-passwall
+#rm -rf feeds/luci/applications/openwrt-passwall
+rm -rf feeds/luci/applications/luci-app-passwal
+rm -rf feeds/luci/applications/luci-app-mihomo
 
 # 修改默认主题
 #sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
