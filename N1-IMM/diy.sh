ac9e4e1ee01040caf8db1c1f12e17ea5b3f3badb
sck03407 <138302895+sck03407@users.noreply.github.com>
2025-01-17 09:35:02 +0800

    Create diy.sh

---
+++ N1-IMM/diy.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# Remove packages
+#rm -rf feeds/packages/net/v2ray-geodata
+
+
+# Add packages
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo package/luci-app-mihomo
+
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
+#git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+
+# 加入OpenClash核心
+#chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
+#$GITHUB_WORKSPACE/N1/preset-clash-core.sh
+
+#echo "
+# 插件
+#CONFIG_PACKAGE_luci-app-lucky=y
+#" >> .config
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.6.6/g' package/base-files/files/bin/config_generate
+
+# 清理软件包
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+#rm -rf feeds/luci/applications/openwrt-passwall
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-mihomo
+
+# 修改默认主题
+#sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 修改主机名
+#sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+
+# mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
