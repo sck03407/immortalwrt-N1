49272977bdf0f9bc454d868201b99583f06a93a4
sck03407 <138302895+sck03407@users.noreply.github.com>
2025-01-25 10:29:55 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -66,7 +66,7 @@ git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages packa
 # mosdns
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+rm -rf feeds/packages/net/v2ray-geodata
+#find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
